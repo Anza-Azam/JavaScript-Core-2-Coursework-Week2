98e72a172cc50918c807f81c020292f78b036ad2
Anza-Azam <anzaazam.nw4@gmail.com>
2021-07-25 18:22:05 -0700

    D & E

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,30 @@
-function todoList(todos) {
+function todoList (todos) {
   // Write your code here...
+
+  const content = document.querySelector('#content')
+
+  const todoList = document.createElement('ul')
+  content.appendChild(todoList)
+
+  todos.forEach(task => {
+    const items = document.createElement('li')
+    items.innerHTML = task.todo
+
+    todoList.appendChild(items)
+
+    items.addEventListener('click', () => {
+      if (items.style.textDecoration === 'line-through')
+        items.style.textDecoration = 'none'
+      else items.style.textDecoration = 'line-through'
+    })
+  })
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
-];
+  { todo: 'wash the dishes' },
+  { todo: 'walk the dog' },
+  { todo: 'learn javascript' },
+  { todo: 'go shopping' }
+]
 
-todoList(todos);
+todoList(todos)
--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,7 +1,32 @@
-function listOfColours(colours) {
+function listOfColours (colours) {
   // Write your code here...
+
+  var content = document.querySelector('#content')
+
+  const colors = document.createElement('select')
+  content.appendChild(colors)
+  const colorN = document.createElement('p')
+  content.appendChild(colorN)
+
+  colours.forEach(colorName => {
+    let opt = document.createElement('option')
+    colors.appendChild(opt)
+    opt.innerText = colorName
+    colors.addEventListener('click', e => {
+      console.log(e)
+      colorN.innerText = `You have selected: ${e.target.value}`
+      colorN.style.color = e.target.value
+    })
+  })
 }
 
-const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
+const colours = ['red', 'blue', 'green', 'yellow', 'pink', 'brown']
 
-listOfColours(colours);
+listOfColours(colours)
+
+// * Create a `<select>` element.
+// * Create a `<p>` element.
+// * Iterate through the array of colours.
+// * Each element in the array should add a `<option>` to the `<select>`, where the `<option>` is the element in the array.
+// * Each `<option>` should have a 'click' event listener to update the contents and colour of the `<p>` with the selected colour.
+// * All of your HTML should go inside the `<div>` with the id **"content"**.
