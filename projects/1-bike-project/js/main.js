eadbf1778d20e64ae0eae66b1e1299770994de3a
Anza-Azam <anzaazam.nw4@gmail.com>
2021-07-28 16:12:43 -0700

    projects done

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -1,56 +1,47 @@
 /** @format */
 
-//////Blue Button
+/////First Part
+
 const blueBtn = document.getElementById('blueBtn')
 const orangeBtn = document.getElementById('orangeBtn')
 const greenBtn = document.getElementById('greenBtn')
-
+const donateBikeBtn = document.querySelector('.btn.btn-primary.btn-lrg')
+const volunteerBtn = document.querySelector('.btn.btn-secondary.btn-lrg')
 const jumbotron = document.querySelector('.jumbotron')
+
+//////Blue Button
+
 blueBtn.addEventListener('click', () => {
   jumbotron.style.backgroundColor = '#588fbd'
-  const donateBikeBtn = document.querySelector('.btn.btn-primary.btn-lrg')
   donateBikeBtn.style.backgroundColor = '#ffa500'
-  console.log(donateBikeBtn.style.backgroundColor)
-  const volunteerBtn = document.querySelector('.btn.btn-secondary.btn-lrg')
   volunteerBtn.style.backgroundColor = '#000000'
   volunteerBtn.style.color = '#ffffff'
 })
-
+//orange button
 orangeBtn.addEventListener('click', () => {
   jumbotron.style.backgroundColor = '#f0ad4e'
-  const donateBikeBtn = document.querySelector('.btn.btn-primary.btn-lrg')
   donateBikeBtn.style.backgroundColor = '#5751fd'
-  console.log(donateBikeBtn.style.backgroundColor)
-  const volunteerBtn = document.querySelector('.btn.btn-secondary.btn-lrg')
   volunteerBtn.style.backgroundColor = '#31b0d5'
   volunteerBtn.style.color = '#ffffff'
 })
-
-
+//green button
 greenBtn.addEventListener('click', () => {
   jumbotron.style.backgroundColor = '#87ca8a'
-  const donateBikeBtn = document.querySelector('.btn.btn-primary.btn-lrg')
   donateBikeBtn.style.backgroundColor = 'black'
-  console.log(donateBikeBtn.style.backgroundColor)
-  const volunteerBtn = document.querySelector('.btn.btn-secondary.btn-lrg')
   volunteerBtn.style.backgroundColor = `#8c9c08`
-
   volunteerBtn.style.color = '#ffffff'
 })
 
-//////form functionality
+//////form functionality       second part
 
 const submit = document.querySelector('button:last-child')
 
 submit.addEventListener('click', validation)
- 
 
-
-
-function validation(event)
-{event.preventDefault()
+function validation (event) {
+  event.preventDefault()
   const formFields = document.querySelectorAll('form .form-control')
- 
+
   let valid = []
   const array = Array.from(formFields)
   array.forEach((Element, index) => {
@@ -76,8 +67,6 @@ function validation(event)
         Element.value = ''
         valid.pop()
         Element.removeAttribute('style')
-
-        
       })
     }
   })
--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,101 +1,90 @@
 /** @format */
+let list = document.getElementById('todo-list')
 
-function populateTodoList(todos) {
-	let list = document.getElementById("todo-list");
+function populateTodoList (todos) {
+  todos.forEach(element => {
+    let li = document.createElement('li')
+    li.className =
+      'list-group-item d-flex justify-content-between align-items-center'
+    list.appendChild(li)
 
-	todos.forEach((element) => {
-		let li = document.createElement("li");
-		li.className =
-			"list-group-item d-flex justify-content-between align-items-center";
-		list.appendChild(li);
+    let span1 = document.createElement('span')
+    span1.className = 'badge bg-primary rounded-pill'
 
-		let span1 = document.createElement("span");
-		span1.className = "badge bg-primary rounded-pill";
+    li.innerText = element.task
+    let i1 = document.createElement('i')
+    let i2 = document.createElement('i')
 
-		li.innerText = element.task;
-		let i1 = document.createElement("i");
-		let i2 = document.createElement("i");
+    i1.className = 'fa fa-check'
+    i2.className = 'fa fa-trash'
+    li.appendChild(span1)
+    span1.append(i1, i2)
 
-		i1.className = "fa fa-check";
-		i2.className = "fa fa-trash";
-		li.appendChild(span1);
-		span1.append(i1, i2);
+    i1.addEventListener('click', () => {
+      li.classList.toggle('line-through')
+    })
+    i2.addEventListener('click', () => {
+      console.log(element)
 
-		i1.addEventListener("click", () => {
-			console.log(element);
-			if (li.style.textDecoration === "line-through")
-				li.removeAttribute("style");
-			else li.style.textDecoration = "line-through";
-		});
-		i2.addEventListener("click", () => {
-			console.log(element);
-		
-			list.removeChild(li);
-		
-		});
-	});
+      list.removeChild(li)
+    })
+  })
 
-	// Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 }
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
 let todos = [
-	{ task: "Wash the dishes", completed: false },
-	{ task: "Do the shopping", completed: false },
-];
+  { task: 'Wash the dishes', completed: false },
+  { task: 'Do the shopping', completed: false }
+]
 
-//console.log(todos)
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 
-const submit = document.querySelector(".btn.btn-primary.mb-3");
+const submit = document.querySelector('.btn.btn-primary.mb-3')
+const deleteB = document.createElement('button')
+deleteB.className = 'button'
+const creates = document.querySelector('.col-auto')
+creates.appendChild(deleteB)
+deleteB.innerText = 'Delete All Tasks'
 
-function addNewTodo(event) {
-	let list = document.getElementById("todo-list");
+function addNewTodo (event) {
+  let newTask = []
 
-	event.preventDefault();
-	let inputBox = document.querySelector("#todoInput");
-	let title = inputBox.value;
-	console.log(title);
-	todos.task = title;
-	todos.push({ task: title, completed: false });
-	console.log(todos);
-	console.log(event);
-	let li = document.createElement("li");
-	li.className =
-		"list-group-item d-flex justify-content-between align-items-center";
-	list.appendChild(li);
+  event.preventDefault()
+  let inputBox = document.querySelector('#todoInput')
+  let title = inputBox.value
+  newTask.push({ task: title, completed: false })
+  populateTodoList(newTask)
 
-	let span1 = document.createElement("span");
-	span1.className = "badge bg-primary rounded-pill";
-
-	li.innerText = title;
-	let i1 = document.createElement("i");
-	let i2 = document.createElement("i");
-
-	i1.className = "fa fa-check";
-	i2.className = "fa fa-trash";
-	li.appendChild(span1);
-	span1.append(i1, i2);
-
-	i1.addEventListener("click", () => {
-		//console.log(element)
-		if (li.style.textDecoration === "line-through")
-			li.removeAttribute("style");
-		else li.style.textDecoration = "line-through";
-	});
-	i2.addEventListener("click", () => {
-		list.removeChild(li);
-	});
+  inputBox.value = ''
 }
 
 // The code below prevents the page from refreshing when we click the 'Add Todo' button.
 
 // Write your code here... and remember to reset the input field to be blank after creating a todo!
 
-populateTodoList(todos);
+populateTodoList(todos)
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
-function deleteAllCompletedTodos() {
-	// Write your code here...
+
+deleteB.addEventListener('click', deleteAllCompletedTodos)
+
+function deleteAllCompletedTodos (event) {
+  event.preventDefault()
+  // Write your code here...
+
+  let alltodos = document.querySelectorAll(
+    '.list-group-item.d-flex.justify-content-between.align-items-center'
+  )
+
+  let todoList = Array.from(alltodos)
+
+  todoList.forEach(element => {
+    console.log(element)
+    if (element.classList.contains('line-through')) {
+      element.parentElement.removeChild(element)
+    }
+  })
 }
